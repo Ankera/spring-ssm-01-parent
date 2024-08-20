c23a2b07e058cabd19bb165350d41229be4b0343
ankera <15189120919@163.com>
2024-08-21 00:45:22 +0800

    feat: init parent

---
+++ README.md
@@ -0,0 +1,16 @@
+# spring-ssm-01-parent
+
+## spring 框架
+
+### 05-Springmvc高级
+
+#### 
+```text
+<packaging>pom</packaging>
+
+<modules>
+    <module>../spring-ssm-01</module>
+    <module>../spring-ssm-01-po</module>
+    <module>../spring-ssm-01-dao</module>
+</modules>
+```
---
+++ src/main/java/com/ankers/SpringSsm01ParentApplication.java
@@ -0,0 +1,13 @@
+package com.ankers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSsm01ParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSsm01ParentApplication.class, args);
+    }
+
+}
